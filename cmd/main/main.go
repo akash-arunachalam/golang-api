48b4b16efbf7197a91765fe256e3b5168bb690a8
akshaya s <akshaya.s@marketsimplified.com>
2022-08-16 19:53:39 +0530

    fix

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,12 +1,17 @@
 package main
-import("log"
-"net/http"
-"github.com/gorilla/mux"
-_ "github.com/jinzhu/gorm/dialects/mysql"
-"golang-api/pkg/routes")
-func main(){
-r:=mux.NewRouter()
-routes.RegisterBookstoreRoutes(r)
-http.Handle("/",r)
-log.Fatal(http.ListenAndServe("localhost:9010",r))
+
+import (
+	"golang-api/pkg/routes"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookstoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }
