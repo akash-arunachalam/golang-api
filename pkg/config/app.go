d19702bfe512f21321a771ee703387e189835a19
akshaya s <akshaya.s@marketsimplified.com>
2022-08-16 20:35:28 +0530

    fix

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "akash:Akash#99@tcp(34.93.109.172:3306)/billing?charset=utf8&parseTime=true&loc=Local")
+	d, err := gorm.Open("mysql", "akash:Akash#99@tcp(34.93.109.172:3306)/ak?charset=utf8&parseTime=true&loc=Local")
 	if err != nil {
 		panic(err)
 	}
